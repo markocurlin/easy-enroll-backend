5d43b8b829639772cd16090a825ef4d89aca4128
Marko <marko.curlin22@gmail.com>
2024-01-16 22:59:05 +0100

    Working on CORS

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
+
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
