9370c59a8dbc535f8a9fd323a5e86b69a916edfd
Marko <marko.curlin22@gmail.com>
2024-01-16 22:51:27 +0100

    Added response headers

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const cors = require('cors');
 const helmet = require('helmet');
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
