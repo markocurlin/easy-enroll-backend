d6d2335cc558a73c91c795344dca42c55eae88e2
Marko <marko.curlin22@gmail.com>
2024-01-16 22:36:38 +0100

    Added cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const helmet = require('helmet');
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
