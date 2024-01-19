d33c60e3cb89e86139c1dd0ea49acd75a888d363
Marko <marko.curlin22@gmail.com>
2024-01-19 22:59:23 +0100

    Working on db connection, testing old packages

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-//import { MongoClient } from 'mongodb';
 require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
-//const mongoose = require('./node_modules/mongoose');
-
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
 
@@ -20,21 +17,7 @@ app.use('/', routes);
 
 app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
 
-const DATABASE_URI = "mongodb+srv://mcurli00:fesb1234@cluster0.cig6cob.mongodb.net/easy-enroll-db"
-
-//const client = new MongoClient(DATABASE_URI);
-//mongoose.connect(process.env.DATABASE_URI);
-//mongoose.connect(DATABASE_URI);
-
-/*
-client.connect((error) => {
-    if (error) {
-        console.log(`⚡️[DB]: Could not connected to database!`);
-        console.log(error)     
-    } else {
-        console.log(`⚡️[DB]: Connected to database!`);
-    }
-});
+mongoose.connect(process.env.DATABASE_URI);
 
 const db = mongoose.connection;
 
@@ -45,5 +28,5 @@ db.once('open', () => {
 db.on('error', () => {
   console.log('Database connection error.');
 });
-*/
+
 module.exports = app;
